5f33802d369eb1aee602829ff0600b7bb9cc5646
makary-yasser <makarydev19@gmail.com>
2024-07-12 00:52:56 +0300

    small update

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -3,12 +3,12 @@
 const LandingPage = () => {
   return (
     <section className="rounded-b-2xl pt-40 px-5 lg:px-32 pb-10 mb-20 h-[95vh] landing-bg drop-shadow-sm">
-      <h2 className="md:text-6xl text-5xl text-wrap uppercase w-5/6 text-white leading-tight tracking-wide">
+      <h1 className="lg:text-6xl text-4xl text-wrap uppercase lg:w-5/6 text-white leading-tight tracking-wide">
         Modern Construction Company
-      </h2>
-      <h1 className="text-3xl text-wrap mt-5 mb-10 lg:w-3/4 leading-tight text-white">
-        We Build Your Dreams
       </h1>
+      <h2 className="text-3xl text-wrap mt-5 mb-10 lg:w-3/4 leading-tight text-white">
+        We Build Your Dreams
+      </h2>
       
     </section>
   );
