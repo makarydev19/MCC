575d79131f518d0ab6cf283c725df04429529167
makary-yasser <makarydev19@gmail.com>
2024-11-25 17:00:31 +0200

    upload logo icon

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,28 +1,28 @@
-import type { Metadata } from "next";
-import { Roboto, Inter } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
-import ThemeProvider from "@/components/ThemeProvider/theme-provider";
-import { Toaster } from "@/components/ui/toaster";
+import type { Metadata } from 'next';
+import { Roboto, Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+import ThemeProvider from '@/components/ThemeProvider/theme-provider';
+import { Toaster } from '@/components/ui/toaster';
 
 const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500", "700", "900"],
-  style: ["italic", "normal"],
-  variable: "--font-roboto",
+  subsets: ['latin'],
+  weight: ['400', '500', '700', '900'],
+  style: ['italic', 'normal'],
+  variable: '--font-roboto',
 });
 
 const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-  style: ["normal"],
-  variable: "--font-inter",
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+  style: ['normal'],
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
-  title: "Modern Construction Company",
-  description: "WE BUILD YOUR DREAMS",
+  title: 'Modern Construction Company',
+  description: 'WE BUILD YOUR DREAMS',
 };
 
 export default function RootLayout({
@@ -32,6 +32,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`scroll-smooth ${inter.variable}`}>
+      <head>
+        <link
+          rel="shortcut icon"
+          href="/MCC_Logo-removebg-preview.png"
+          type="image/png"
+        />
+      </head>
       <body className={roboto.className}>
         <ThemeProvider>
           <main className="font-normal">
